9ddf6c11b2a0a14d9f5cba4c27dd0764b191488f
Eric Wiese <eric.wiese@classyllama.com>
2014-10-10 08:58:54 -0500

    Add admin menu and ACL

--- app/code/community/EW/UntranslatedStrings/controllers/Adminhtml/UntranslatedController.php
+++ app/code/community/EW/UntranslatedStrings/controllers/Adminhtml/UntranslatedController.php
@@ -2,8 +2,25 @@
 
 class EW_UntranslatedStrings_Adminhtml_UntranslatedController extends Mage_Adminhtml_Controller_Action
 {
-    public function indexAction() {
+    public function reportAction() {
         $this->loadLayout();
         $this->renderLayout();
+    }
+
+    /**
+     * Enforce ACL
+     *
+     * @return bool
+     */
+    protected function _isAllowed()
+    {
+        switch ($this->getRequest()->getActionName()) {
+            case 'report':
+                return Mage::getSingleton('admin/session')->isAllowed('report/ew_untranslatedstrings/report');
+                break;
+            default:
+                return Mage::getSingleton('admin/session')->isAllowed('report/ew_untranslatedstrings');
+                break;
+        }
     }
 }
